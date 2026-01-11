8db82d0ae15acdc960f0b1658b2d23cd96c5409f
Crafted <banditkingharper@gmail.com>
2026-01-11 14:51:39 -0700

    Update index.js

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,5 +1,20 @@
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+
 export default {
-  async fetch(request) {
-    return new Response("Worker entry-point placeholder", { status: 200 });
-  }
+  async fetch(event) {
+    const manifest = JSON.parse(manifestJSON);
+    let url = new URL(event.request.url);
+    let path = url.pathname;
+
+    if (path === "/") path = "/index.html";
+    path = path.replace(/^\/+/, ""); // remove leading /
+
+    if (manifest[path]) {
+      const file = manifest[path];
+      return fetch(file);
+    }
+
+    // fallback to index.html for SPA routes
+    return fetch(manifest['index.html']);
+  },
 };
